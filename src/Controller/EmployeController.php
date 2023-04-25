907b58db878439fb1e9f99761bcd70993542aa8b
Littlebirdwarrior <camille.dorland@gmail.com>
2023-04-25 16:28:00 +0200

    bug employe add resolut

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class EmployeController extends AbstractController
 {
